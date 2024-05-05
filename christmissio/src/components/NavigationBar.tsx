d71f4d1518c49c685b5fa64ec78ad5fa6dcab4a8
emperorbj <cwaku96@gmail.com>
2024-05-05 03:34:54 -0700

    created app with its components and pages

---
+++ christmissio/src/components/NavigationBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const NavigationBar = () => {
+    return (
+        <nav className='bg-purple-400 w-full h-[70px]'>
+            <div>
+                Logo
+            </div>
+            <div>
+                <Link to=''>
+                    <button>Home</button>
+                </Link>
+
+                <Link to=''>
+                    <button>About Us</button>
+                </Link>
+
+                <Link to=''>
+                    <button>Contact Us</button>
+                </Link>
+
+                <Link to=''>
+                    <button>Events</button>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default NavigationBar

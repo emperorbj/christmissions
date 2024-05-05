62f7883b3a7d4446d296a5282c28b6b80b032855
emperorbj <cwaku96@gmail.com>
2024-05-05 03:40:49 -0700

    navigation bar

--- christmissio/src/components/NavigationBar.tsx
+++ christmissio/src/components/NavigationBar.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 
 const NavigationBar = () => {
     return (
+        // navigation bar
         <nav className='bg-purple-400 w-full h-[70px]'>
             <div>
                 Logo
